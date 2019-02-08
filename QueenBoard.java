a5f1d152372fa693437da093b498326494ba7061
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-02-08 10:09:15 -0500

    fixed markAttack method to subtract from square rather than set to -1. copied to removeAttack but replaced -- with ++. testing.

--- QueenBoard.java
+++ QueenBoard.java
@@ -10,50 +10,96 @@ public class QueenBoard {
 
   private boolean addQueen(int r, int c) {
     board[r][c] = 1;
+    markAttack(r,c);
     return true;
   }
   private boolean removeQueen(int r, int c) {
     board[r][c] = 0;
     return true;
   }
+
   //this method marks the lines of attack for a queen placed at r, c.
+  //a square under attack will have value < 0 and will be -1 * # of times being attacked.
   private boolean markAttack(int r, int c) {
     int rows = board.length;
     int cols = board[0].length;
     //vertical
     for (int[] row: board) {
       if (row[c] != 1) {
-        row[c] = -1;
+        row[c]--;
       }
     }
     //horizontal.
     for (int i=0;i<board[r].length;i++) {
       if (board[r][i] != 1) {
-        board[r][i] = -1;
+        board[r][i]--;
       }
     }
     //diagonal up right./>
     for (int i=r,i2=c;i>=0&&i2<board[r].length;i--,i2++) {
       if (board[i][i2] != 1) {
-        board[i][i2] = -1;
+        board[i][i2]--;
       }
     }
     //diagonal down left.</
     for (int i=r,i2=c;i2>=0&&i<board[r].length;i++,i2--) {
       if (board[i][i2] != 1) {
-        board[i][i2] = -1;
+        board[i][i2]--;
       }
     }
     //diagonal up left <\
     for (int i=r,i2=c;i>=0&&i2>=0;i--,i2--) {
       if (board[i][i2] != 1) {
-        board[i][i2] = -1;
+        board[i][i2]--;
       }
     }
     //diagonal down left \>
     for (int i=r,i2=c;i<board[r].length&&i2<board[r].length;i++,i2++) {
       if (board[i][i2] != 1) {
-        board[i][i2] = -1;
+        board[i][i2]--;
+      }
+    }
+    return true;
+  }
+
+  //given r,c of queen just removed fix the board to make areas now safe.
+  private boolean removeAttack(int r, int c) {
+    int rows = board.length;
+    int cols = board[0].length;
+    //vertical
+    for (int[] row: board) {
+      if (row[c] != 1) {
+        row[c]++;
+      }
+    }
+    //horizontal.
+    for (int i=0;i<board[r].length;i++) {
+      if (board[r][i] != 1) {
+        board[r][i]++;
+      }
+    }
+    //diagonal up right./>
+    for (int i=r,i2=c;i>=0&&i2<board[r].length;i--,i2++) {
+      if (board[i][i2] != 1) {
+        board[i][i2]++;
+      }
+    }
+    //diagonal down left.</
+    for (int i=r,i2=c;i2>=0&&i<board[r].length;i++,i2--) {
+      if (board[i][i2] != 1) {
+        board[i][i2]++;
+      }
+    }
+    //diagonal up left <\
+    for (int i=r,i2=c;i>=0&&i2>=0;i--,i2--) {
+      if (board[i][i2] != 1) {
+        board[i][i2]++;
+      }
+    }
+    //diagonal down left \>
+    for (int i=r,i2=c;i<board[r].length&&i2<board[r].length;i++,i2++) {
+      if (board[i][i2] != 1) {
+        board[i][i2]++;
       }
     }
     return true;
@@ -78,12 +124,16 @@ public class QueenBoard {
   }
 
   public boolean solve() {
+    int[][] queens = new int[8][2];
     addQueen(0,0);
     return true;
 
   }
 
-  public boolean solve(int row, int col){
+  public boolean solve(int row, int col, int[] queens){
+    if (col==board[row].length) {
+      return false;
+    }
     return true;
   }
 
